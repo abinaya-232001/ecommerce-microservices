02e921315b42c3eb284f8889524496eb0ca68b2b
Savin De Silva <144529221+SavinDeSilva@users.noreply.github.com>
2026-03-30 18:43:46 +0530

    Delete order_service directory

--- order_service/data.py
+++
@@ -1,5 +0,0 @@
-orders = [
-{"id": 1, "customer_id": 101, "items": ["item1"], "total_price": 100, "status": "pending"},
-{"id": 2, "customer_id": 102, "items": ["item2"], "total_price": 200, "status": "shipped"},
-{"id": 3, "customer_id": 103, "items": ["item3"], "total_price": 300, "status": "delivered"}
-]
--- order_service/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class Order(BaseModel):
-    id: int | None = None
-    customer_id: int
-    items: list[str]
-    total_price: float
-    status: str
